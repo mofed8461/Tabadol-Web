78b78dae177ddf488d7680250720dfcd9ee42405
hamzashakah <hamzashakah95@gmail.com>
2018-08-17 23:04:41 +0300

    admin login page and edit db

---
+++ tabadolweb/connect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamza
+ * Date: 4/13/2018
+ * Time: 5:49 PM
+ */
+$con = new mysqli("localhost" , "root" , "" ,"tabadolweb") or die("error in connect dataBase");
+
+
+
+
+
+?>
