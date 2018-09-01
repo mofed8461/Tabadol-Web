1b628dcd294e941322d73b089edf3804fd17d336
hamzashakah <hamzashakah95@gmail.com>
2018-09-01 22:56:50 +0300

    tt

--- tabadolweb/reportform.php
+++ tabadolweb/reportform.php
@@ -8,21 +8,21 @@
 <body >
 	<div align="center">
 <img src="imgs/Screen Shot 2018-09-01 at 9.36.23 PM.png"/>
-		<input style="position: absolute; left: 487px; top: 518px; width: 225px;" type="text" name="school_name"/>
-	  <input style="position: absolute; left: 29px; top: 519px; width: 159px;" type="text" name="phone"/>
-	  <input style="position: absolute; left: 480px; top: 591px; width: 212px;" type="text" name="person1"/>
-		<input style="position: absolute; left: 44px; top: 592px; width: 212px;" type="text" name="phone1"/>
+		<input style="position: absolute; left: 533px; top: 517px; width: 225px;" type="text" name="school_name"/>
+	  <input style="position: absolute; left: 92px; top: 519px; width: 159px;" type="text" name="phone"/>
+	  <input style="position: absolute; left: 538px; top: 594px; width: 212px;" type="text" name="person1"/>
+		<input style="position: absolute; left: 105px; top: 590px; width: 212px;" type="text" name="phone1"/>
 		<!--<input style="position: absolute; left: 472px; top: 666px; width: 212px;" type="text" name="school_name"/>-->
-		<input style="position: absolute; left: 30px; top: 669px; width: 212px;" type="text" name="manager1"/>
-		<input style="position: absolute; left: 507px; top: 821px; width: 212px; height: 26px;" type="text" name="school_name2"/>
-	  <input style="position: absolute; left: 91px; top: 822px; width: 142px; height: 26px;" type="text" name="phone2"/>
-	  <input style="position: absolute; left: 460px; top: 894px; width: 160px; height: 26px;" type="text" name="person3"/>
-	  <input style="position: absolute; left: 93px; top: 893px; width: 212px; height: 26px;" type="text" name="phone3"/>
-	  <input style="position: absolute; left: 120px; top: 966px; width: 159px; height: 26px;" type="text" name="manager3"/>
-	  <input style="position: absolute; left: 546px; top: 1123px; width: 191px; height: 26px;" type="text" name="dev_name"/>
+		<input style="position: absolute; left: 89px; top: 667px; width: 212px;" type="text" name="manager1"/>
+		<input style="position: absolute; left: 564px; top: 816px; width: 212px; height: 26px;" type="text" name="school_name2"/>
+	  <input style="position: absolute; left: 140px; top: 815px; width: 142px; height: 26px;" type="text" name="phone2"/>
+	  <input style="position: absolute; left: 540px; top: 889px; width: 160px; height: 26px;" type="text" name="person3"/>
+	  <input style="position: absolute; left: 164px; top: 892px; width: 212px; height: 26px;" type="text" name="phone3"/>
+	  <input style="position: absolute; left: 192px; top: 964px; width: 159px; height: 26px;" type="text" name="manager3"/>
+	  <input style="position: absolute; left: 602px; top: 1127px; width: 191px; height: 26px;" type="text" name="dev_name"/>
 		<!--<input style="position: absolute; left: 525px; top: 1194px; width: 159px; height: 26px;" type="text" name="school_name"/>-->
-		<input style="position: absolute; left: 541px; top: 1273px; width: 159px; height: 26px;" type="text" name="from"/>
-		<input style="position: absolute; left: 156px; top: 1270px; width: 219px; height: 26px;" type="text" name="to"/>
+		<input style="position: absolute; left: 601px; top: 1275px; width: 159px; height: 26px;" type="text" name="from"/>
+		<input style="position: absolute; left: 232px; top: 1271px; width: 219px; height: 26px;" type="text" name="to"/>
 	<!--	<input style="position: absolute; left: 457px; top: 965px; width: 159px; height: 26px;" type="text" name="school_name"/>-->
 </div>
 </body>
